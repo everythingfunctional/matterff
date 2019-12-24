f387dc68e0a698d819eefea102aeaa8f33592ccd
Brad Richardson <brichardson@structint.com>
2019-12-23 19:05:38 -0800

    Enable finding an isotope in a list

--- src/Isotope_m.f90
+++ src/Isotope_m.f90
@@ -1,7 +1,8 @@
 module Isotope_m
     use Element_symbol_m, only: ElementSymbol_t
     use iso_varying_string, only: VARYING_STRING
-    use Isotope_symbol_m, only: IsotopeSymbol_t, H_1_SYM, He_3_SYM
+    use Isotope_symbol_m, only: &
+            IsotopeSymbol_t, H_1_SYM, H_2_SYM, H_3_SYM, He_3_SYM, He_4_SYM
     use quaff, only: MolarMass_t
 
     implicit none
@@ -22,8 +23,13 @@ module Isotope_m
     ! atomic mass is taken from:
     ! [1] https://physics.nist.gov/cgi-bin/Compositions/stand_alone.pl
     type(Isotope_t), parameter, public :: H_1 = Isotope_t(H_1_SYM, MolarMass_t(kilograms_per_mol = 1.0078250321d-3))
+    type(Isotope_t), parameter, public :: H_2 = Isotope_t(H_2_SYM, MolarMass_t(kilograms_per_mol = 2.0141017779d-3))
+    type(Isotope_t), parameter, public :: H_3 = Isotope_t(H_3_SYM, MolarMass_t(kilograms_per_mol = 3.0160492779d-3)) ! [1]
 
     type(Isotope_t), parameter, public :: He_3 = Isotope_t(He_3_SYM, MolarMass_t(kilograms_per_mol = 3.016029319d-3))
+    type(Isotope_t), parameter, public :: He_4 = Isotope_t(He_4_SYM, MolarMass_t(kilograms_per_mol = 4.0026032542d-3))
+
+    public :: find
 contains
     elemental function is(self, element)
         class(Isotope_t), intent(in) :: self
@@ -39,4 +45,20 @@ contains
 
         string = self%symbol%toString()
     end function toString
+
+    pure function find(symbol, isotopes) result(position)
+        type(IsotopeSymbol_t), intent(in) :: symbol
+        type(Isotope_t), intent(in) :: isotopes(:)
+        integer :: position
+
+        integer :: i
+
+        position = 0
+        do i = 1, size(isotopes)
+            if (isotopes(i)%symbol == symbol) then
+                position = i
+                exit
+            end if
+        end do
+    end function find
 end module Isotope_m
--- tests/isotope_test.f90
+++ tests/isotope_test.f90
@@ -1,9 +1,16 @@
 module isotope_test
     use Element_symbol_m, only: H, He
     use iso_varying_string, only: operator(//)
-    use Isotope_m, only: H_1
+    use Isotope_m, only: Isotope_t, find, H_1, H_2, He_3, He_4
+    use Isotope_symbol_m, only: H_1_SYM, H_2_SYM
     use Vegetables_m, only: &
-            Result_t, TestItem_t, assertNot, assertThat, Describe, It
+            Result_t, &
+            TestItem_t, &
+            assertEquals, &
+            assertNot, &
+            assertThat, &
+            Describe, &
+            It
 
     implicit none
     private
@@ -13,11 +20,15 @@ contains
     function test_isotope() result(tests)
         type(TestItem_t) :: tests
 
-        type(TestItem_t) :: individual_tests(2)
+        type(TestItem_t) :: individual_tests(4)
 
         individual_tests(1) = It("Is it's element", checkIsElement)
         individual_tests(2) = It( &
                 "Is not a different element", checkNotDifferentElement)
+        individual_tests(3) = It( &
+                "Has a position of 0 if it's not in a list", checkNotFound)
+        individual_tests(4) = It( &
+                "Can be found in a list", checkFind)
         tests = Describe("Isotope_t", individual_tests)
     end function test_isotope
 
@@ -34,4 +45,20 @@ contains
         result_ = assertNot( &
                 H_1%is(He), H_1%toString() // "%is(" // He%toString() // ")")
     end function checkNotDifferentElement
+
+    pure function checkNotFound() result(result_)
+        type(Result_t) :: result_
+
+        type(Isotope_t), parameter :: isotopes(*) = [H_2, He_3, He_4]
+
+        result_ = assertEquals(0, find(H_1_SYM, isotopes))
+    end function checkNotFound
+
+    pure function checkFind() result(result_)
+        type(Result_t) :: result_
+
+        type(Isotope_t), parameter :: isotopes(*) = [H_1, H_2, He_3, He_4]
+
+        result_ = assertEquals(2, find(H_2_SYM, isotopes))
+    end function checkFind
 end module isotope_test
