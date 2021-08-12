3e5a8c8f26fde8269344f99f561d4896e4e066a9
Brad Richardson <everythingfunctional@protonmail.com>
2021-08-12 17:34:37 -0500

    test(isotope): error conditions in JSON

--- test/isotope_json_test.f90
+++ test/isotope_json_test.f90
@@ -1,11 +1,11 @@
 module isotope_json_test
-    use erloff, only: error_list_t
+    use erloff, only: error_list_t, NOT_FOUND, UNKNOWN_TYPE
     use iso_varying_string, only: operator(//)
     use jsonff, only: fallible_json_value_t, json_object_t, parse_json
-    use matterff, only: fallible_isotope_t, isotope_t, H_1
+    use matterff, only: fallible_isotope_t, isotope_t, H_1, INVALID_ARGUMENT
     use strff, only: NEWLINE
     use vegetables, only: &
-            result_t, test_item_t, assert_equals, describe, fail, it
+            result_t, test_item_t, assert_equals, assert_that, describe, fail, it
 
     implicit none
     private
@@ -18,6 +18,9 @@ contains
                 "an isotope", &
                 [ it("can be converted to JSON", check_convert_to_json) &
                 , it("can be extracted from JSON", check_extract_from_json) &
+                , it("is an error if the isotope key is missing", check_missing_isotope_entry) &
+                , it("receives the error for an invalid isotope", check_invalid_isotope) &
+                , it("catches an invalid json type", check_invalid_type) &
                 ])
     end function
 
@@ -65,6 +68,95 @@ contains
                     isotope = maybe_isotope%isotope()
                     result_ = assert_equals(H_1%to_string(), isotope%to_string())
                 end if
+            class default
+                result_ = fail("Didn't get an object: " // object%to_compact_string())
+            end select
+        end if
+    end function
+
+    function check_missing_isotope_entry() result(result_)
+        type(result_t) :: result_
+
+        character(len=*), parameter :: JSON_STRING = &
+   '{' // NEWLINE &
+// '    "fraction" : 1.0,' // NEWLINE &
+// '    "atomic mass" : "1.0078250321e-3 kg/mol"' // NEWLINE &
+// '}'
+        type(error_list_t) :: errors
+        type(fallible_isotope_t) :: maybe_isotope
+        type(fallible_json_value_t) :: maybe_json
+
+        maybe_json = parse_json(JSON_STRING)
+        if (maybe_json%failed()) then
+            errors = maybe_json%errors()
+            result_ = fail(errors%to_string())
+        else
+            select type (object => maybe_json%value_())
+            type is (json_object_t)
+                maybe_isotope = fallible_isotope_t(object)
+                errors = maybe_isotope%errors()
+                result_ = assert_that( &
+                    errors.hasType.NOT_FOUND, errors%to_string())
+            class default
+                result_ = fail("Didn't get an object: " // object%to_compact_string())
+            end select
+        end if
+    end function
+
+    function check_invalid_isotope() result(result_)
+        type(result_t) :: result_
+
+        character(len=*), parameter :: JSON_STRING = &
+   '{' // NEWLINE &
+// '    "fraction" : 1.0,' // NEWLINE &
+// '    "isotope" : "H-123",' // NEWLINE &
+// '    "atomic mass" : "1.0078250321e-3 kg/mol"' // NEWLINE &
+// '}'
+        type(error_list_t) :: errors
+        type(fallible_isotope_t) :: maybe_isotope
+        type(fallible_json_value_t) :: maybe_json
+
+        maybe_json = parse_json(JSON_STRING)
+        if (maybe_json%failed()) then
+            errors = maybe_json%errors()
+            result_ = fail(errors%to_string())
+        else
+            select type (object => maybe_json%value_())
+            type is (json_object_t)
+                maybe_isotope = fallible_isotope_t(object)
+                errors = maybe_isotope%errors()
+                result_ = assert_that( &
+                    errors.hasType.INVALID_ARGUMENT, errors%to_string())
+            class default
+                result_ = fail("Didn't get an object: " // object%to_compact_string())
+            end select
+        end if
+    end function
+
+    function check_invalid_type() result(result_)
+        type(result_t) :: result_
+
+        character(len=*), parameter :: JSON_STRING = &
+   '{' // NEWLINE &
+// '    "fraction" : 1.0,' // NEWLINE &
+// '    "isotope" : null,' // NEWLINE &
+// '    "atomic mass" : "1.0078250321e-3 kg/mol"' // NEWLINE &
+// '}'
+        type(error_list_t) :: errors
+        type(fallible_isotope_t) :: maybe_isotope
+        type(fallible_json_value_t) :: maybe_json
+
+        maybe_json = parse_json(JSON_STRING)
+        if (maybe_json%failed()) then
+            errors = maybe_json%errors()
+            result_ = fail(errors%to_string())
+        else
+            select type (object => maybe_json%value_())
+            type is (json_object_t)
+                maybe_isotope = fallible_isotope_t(object)
+                errors = maybe_isotope%errors()
+                result_ = assert_that( &
+                    errors.hasType.UNKNOWN_TYPE, errors%to_string())
             class default
                 result_ = fail("Didn't get an object: " // object%to_compact_string())
             end select
